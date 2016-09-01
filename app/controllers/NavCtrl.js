374549c27eb80c368ef8d84ae34779bfb0761f54
Jamie Duke <jamsduke@gmail.com>
2016-09-01 11:12:11 -0500

    navbar now hides when new items clicked

--- app/controllers/NavCtrl.js
+++ app/controllers/NavCtrl.js
@@ -1,12 +1,15 @@
 "use strict";
 
-app.controller("NavCtrl", function($scope, SearchTermData) {
+app.controller("NavCtrl", function($scope, SearchTermData, $location) {
   $scope.searchText = SearchTermData;
   $scope.navItems = [
     {url: "#/logout", name: "Logout"},
     {url: "#/items/list", name: "All Items"},
     {url: "#/items/new", name: "New Items"}
   ];
+
+  $scope.isActive = (viewLocation) => viewLocation === $location.path();
+  // curly brackets and return not needed when writing one line of code after fat arrow
 });
 
 
