5edab64eb795f97b49b006f75d1466cd21f9ceea
Jamie Duke <jamsduke@gmail.com>
2016-09-06 10:58:48 -0500

    app fully functional per instruction

--- app/factories/ItemFactory.js
+++ app/factories/ItemFactory.js
@@ -1,18 +1,24 @@
 "use strict";
 
 app.factory("ItemStorage", ($q, $http, FirebaseURL) => {
-  let getItemList = () => {
+  let getItemList = function(){
     let items = [];
-    return $q((resolve, reject) => {
+    //This is the Angular way of doing promises
+    return $q((resolve, reject)=>{
       $http.get(`${FirebaseURL}/items.json`)
-      .success((itemObject) => {
-        Object.keys(itemObject).forEach((key) => {
-          itemObject[key].id = key;
-          items.push(itemObject[key]);
-        });
-        resolve(items);
+      //Angular does the parsing of the object for you, just like AJAX or getJSON
+      .success((itemObject)=>{
+        if (itemObject !== null){
+          Object.keys(itemObject).forEach((key)=>{
+            itemObject[key].id = key;
+            items.push(itemObject[key]);
+          });
+          resolve(items);
+        } else {
+          resolve(items);
+        }
       })
-      .error((error) => {
+      .error((error)=>{
         reject(error);
       });
     });
